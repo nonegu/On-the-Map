82112793ee4ded94eb9b0f0d17edf5704df841c2
Ender Güzel <enderguzel@gmail.com>
2019-08-05 17:16:13 +0300

    Setup user interface of add location view

---
+++ On the Map/Controller/AddLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ShowLocationViewController.swift
+//  On the Map
+//
+//  Created by Ender Güzel on 5.08.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+    
+}
