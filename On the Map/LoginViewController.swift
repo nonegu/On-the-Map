afa4f626ab866a3fb008b222906b266aae9fb196
Ender Güzel <enderguzel@gmail.com>
2019-08-05 10:59:42 +0300

    Setup user interface of login screen

---
+++ On the Map/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Ender Güzel on 5.08.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+}
+
