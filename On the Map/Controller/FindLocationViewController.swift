278a6ff24d01540cc939e2376233ac23d9f0ca25
Ender Güzel <enderguzel@gmail.com>
2019-08-05 16:33:24 +0300

    Setup user interface of find location view

---
+++ On the Map/Controller/FindLocationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FindLocationViewController.swift
+//  On the Map
+//
+//  Created by Ender Güzel on 5.08.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import UIKit
+
+class FindLocationViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var mediaTextField: UITextField!
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    @IBAction func findLocationPressed(_ sender: UIButton) {
+    }
+    
+}
