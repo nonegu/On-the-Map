1bd9f1a453de52803d8f3c476c236d8c60c3b52e
Ender Güzel <enderguzel@gmail.com>
2019-08-06 14:07:44 +0300

    Add annotation of found Placemark to AddLocationViewController

--- On the Map/Controller/AddLocationViewController.swift
+++ On the Map/Controller/AddLocationViewController.swift
@@ -11,10 +11,39 @@ import MapKit
 
 class AddLocationViewController: UIViewController {
     
+    // MARK: Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
     var placemark: CLPlacemark!
     
     override func viewDidLoad() {
-        print(placemark.location?.coordinate as Any)
+        
+        setAnnotation(with: placemark)
+        setRegion(with: placemark)
+    }
+    
+    func setAnnotation(with placemark: CLPlacemark) {
+        guard let coordinate = placemark.location?.coordinate else {
+            return
+        }
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        DispatchQueue.main.async {
+            self.mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func setRegion(with placemark: CLPlacemark) {
+        guard let coordinate = placemark.location?.coordinate else {
+            return
+        }
+        let userLocation: CLLocationCoordinate2D = coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: userLocation, span: span)
+        
+        DispatchQueue.main.async {
+            self.mapView.setRegion(region, animated: true)
+        }
     }
     
 }
